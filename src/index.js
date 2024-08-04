316c87ac647a215f1d5f5fac22e3d1efbd1ebd3d
avasdeb <avasdeb@gmail.com>
2024-08-04 17:42:10 +0300

    feat: reverce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let a = '';
+    let b = Math.abs(n).toString();
+    for (c = b.length - 1; c >= 0; c--) {
+        a += b[c]
+    };
+    return a;
 }
